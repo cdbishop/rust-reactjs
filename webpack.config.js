bbe3c3d8483d4e91142999b732e145709ce7f1c4
Craig Bishop <craig.bishop@vcatechnology.com>
2020-05-30 14:33:43 +0100

    Update rust + npm dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './frontend/index.js',
-  output: { path: 'static/assets/', filename: 'bundle.js' },
+  output: { path: __dirname + './static/assets/', filename: 'bundle.js' },
   module: {
-    loaders: [
+    rules: [
       {
         test: /.jsx?$/,
         loader: 'babel-loader',
